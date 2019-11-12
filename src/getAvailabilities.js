8713fb621bdbe938d22b3f37c28adbaca17fd1b3
debarghyab <debarghya.pro@gmail.com>
2019-11-13 02:01:06 +0530

    Added methods to handle different event types

--- src/getAvailabilities.js
+++ src/getAvailabilities.js
@@ -37,14 +37,59 @@ export default async function getAvailabilities(date, numberOfDays = 7) {
     ) {
       const day = availabilities.get(date.format("d"));
       if (event.kind === "opening") {
-        day.slots.push(date.format("H:mm"));
+        handleOpenings(event, day);
       } else if (event.kind === "appointment") {
-        day.slots = day.slots.filter(
-          slot => slot.indexOf(date.format("H:mm")) === -1
-        );
+        handleAppointments(event, day);
       }
     }
   }
 
   return Array.from(availabilities.values())
+}
+
+function handleOpenings(event, day){
+  if(event.weekly_recurring){
+    const usefulDates = day.filter(dayDate => moment(dayDate.date) >= date)
+    usefulDates.forEach(dayDate => dayDate.slots.push(date.format("H:mm")))
+  } else {
+    const usefulDate = day.filter(dayDate => moment(dayDate.date) === date);
+    if(usefulDate){
+      usefulDate.slots.push(date.format("H:mm"))
+    }           
+  }
+}
+
+function handleAppointments(event, day){
+  if(event.weekly_recurring){
+    const usefulDates = day.filter(dayDate => compareDateGreater(dayDate.date, date));
+    usefulDates.forEach(dayDate => dayDate.slots = dayDate.slots.filter(
+      slot => slot.indexOf(date.format("H:mm")) === -1
+    ))
+  } else {
+    const usefulDate = day.filter(dayDate => 
+      compareDateEquality(dayDate.date, date.toDate()))[0];
+    if(usefulDate){
+      usefulDate.slots = usefulDate.slots.filter(
+        slot => slot.indexOf(date.format("H:mm")) === -1
+      );
+    }           
+  }
+}
+
+function compareDateEquality(d1, d2) {
+  if(typeof(d1) === typeof(d2)){
+    return d1.getDate() === d2.getDate() 
+    && d1.getMonth() === d2.getMonth() 
+    && d1.getYear() === d2.getYear()
+  }
+  return false;
+}
+
+function compareDateGreater(d1, d2) {
+  if(typeof(d1) === typeof(d2)){
+    return d1.getDate() >= d2.getDate() 
+    || d1.getMonth() >= d2.getMonth() 
+    || d1.getYear() >= d2.getYear()
+  }
+  return false;
 }
